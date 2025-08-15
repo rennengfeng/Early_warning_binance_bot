c23cde2b286fef5e9046fe55ce17a4ee82ba7349
rennengfeng <rennengfeng@gmail.com>
2025-08-15 23:19:16 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# Telegram机器人Token
+TOKEN = "token"  #替换为你的Bot Token
+
+# 授权用户ID列表
+CHAT_IDS = [123456789,525625152]  # 替换为实际用户ID
+
+# Binance API密钥（可选）
+BINANCE_API_KEY = "YOUR_BINANCE_API_KEY"
+BINANCE_API_SECRET = "YOUR_BINANCE_API_SECRET"
+
+# 所有监控使用60分钟K线周期
+DEFAULT_INTERVAL = "60m"
